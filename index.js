a42e3c496ce325e3261d42af589922e146fc2e11
Anshul <anshulsarade@gmail.com>
2024-10-26 02:58:42 +0530

    final update 1

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function addAnimation() {
   });
 }
 
+
+//project images sliding
+const images = ['./Assests/project/img1.jpg', './Assests/project/img2.jpg', './Assests/project/img3.jpg', './Assests/project/img4.jpg','./Assests/project/img5.jpg']; // Array of image sources
+let currentIndex = 0;
+
+
+
+
+function changeImage() {
+  const slideshowImage = document.getElementById('slideshow-image');
+  currentIndex = (currentIndex + 1) % images.length; // Update the index to loop through the images
+  slideshowImage.src = images[currentIndex]; // Change the image source
+}
+
+// Change image every 3 seconds (3000 milliseconds)
+setInterval(changeImage, 3000);
